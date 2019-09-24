7247023f98054da1e6e7ba8ea1ed60443bb3bd3f
David Xiao <50573329+David-Xia0@users.noreply.github.com>
2019-09-25 09:51:34 +1200

    Add files via upload

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
@@ -7,13 +10,13 @@ import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
+	private ExecutorService _team = Executors.newSingleThreadExecutor(); 
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
 		//Initiate the first Scene
-		MainMenu menu = new MainMenu(primaryStage);
-		menu.initiateFileSystem();
-		menu.displayMenu();
+		initiateFileSystem();
 
 		//Sets the whole program to close when application window is closed
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
@@ -27,5 +30,11 @@ public class Main extends Application {
 
 	public static void main(String[] args) {
 		launch();
+	}
+	
+	public void initiateFileSystem() {
+		_team.submit(new RunBash("rm -r ./temp"));
+		_team.submit(new RunBash("mkdir ./VideoCreations ./temp"));
+
 	}
 }
