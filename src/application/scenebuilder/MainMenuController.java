30de26c3956bc9d8786bdb4c05e3f255554b2c59
David Xiao <davidjinxiao@hotmail.com>
2019-09-25 14:18:08 +1200

    added omse more buttons to the home screen

--- src/application/scenebuilder/MainMenuController.java
+++ src/application/scenebuilder/MainMenuController.java
@@ -8,9 +8,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -21,14 +23,28 @@ public class MainMenuController {
 	
 	private ExecutorService _team = Executors.newSingleThreadExecutor(); 
 	private List<String> _creations = new ArrayList<String>();
+    
+    @FXML
+    private Button createButton;
 
     @FXML
     private VBox videoBox;
 
     @FXML
-    private Button createButton;
+    private Button backwardButton;
 
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private Button forwardButton;
+
+    @FXML
+    private Button muteButton;
     
+    @FXML
+    private ListView videoListView;
+
 	{
 		RunBash bash = new RunBash("List=`ls ./VideoCreations` ; List=${List//.???/} ; printf \"${List// /.\\\\n}\\n\"");
 		_team.submit(bash);
@@ -54,6 +70,27 @@ public class MainMenuController {
 		});
 	}
     
+	
+
+    @FXML
+    void handleForward(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleMute(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handlePlay(ActionEvent event) {
+
+    }
+    
+	@FXML
+    void handleBackward(ActionEvent event) {
+
+    }
     
     @FXML
     void handleCreate() {
